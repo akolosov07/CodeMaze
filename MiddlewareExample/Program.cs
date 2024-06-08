74e11a56c8b8f6ca26f90429d0d7dd74aa0b1af2
Alexander Kolosov <avt757@hotmail.com>
2024-06-09 00:35:28 +0300

    init

---
+++ MiddlewareExample/Program.cs
@@ -0,0 +1,62 @@
+using MiddlewareExample.Extensions;
+using NLog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly); ;
+
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+builder.Services.ConfigureCors(); 
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureSqlContext(builder.Configuration);
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+//builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+/*app.Use(async (context, next) => 
+{ 
+    Console.WriteLine($"Logic before executing the next delegate in the Use method"); 
+    await next.Invoke(); 
+    Console.WriteLine($"Logic after executing the next delegate in the Use method"); 
+}); 
+
+app.Map("/usingmapbranch", builder => 
+{ 
+    builder.Use(async (context, next) => 
+    { 
+        Console.WriteLine("Map branch logic in the Use method before the next delegate"); 
+        await next.Invoke(); 
+        Console.WriteLine("Map branch logic in the Use method after the next delegate"); 
+    }); 
+    builder.Run(async context => 
+    { 
+        Console.WriteLine($"Map branch response to the client in the Run method"); 
+        await context.Response.WriteAsync("Hello from the map branch."); 
+    }); 
+}); 
+
+app.Run(async context => 
+{ 
+    Console.WriteLine($"Writing the response to the client in the Run method"); 
+    await context.Response.WriteAsync("Hello from the middleware component."); 
+});*/
+
+app.MapControllers();
+
+app.Run();
